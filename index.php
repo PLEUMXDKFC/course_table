1c4a5b5a51c7150c507c9d12dbf65cde5891da4b
PLEUMXDKFC <kidtapad.gm@gmail.com>
2024-11-27 09:42:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -284,8 +284,7 @@
 
                 <script>
 let checkboxStates = {}; // เก็บสถานะ checkbox แยกตาม filter
-let selectedCheckboxes = new Set(); // เก็บค่า checkbox ที่ถูกเลือก444444444444444444444444444444444
-
+let selectedCheckboxes = new Set(); // เก็บค่า checkbox ที่ถูกเลือก
 
 function filterData() {
     const level = $('#yearSelect').val() || "ทั้งหมด"; // ชั้นปี
